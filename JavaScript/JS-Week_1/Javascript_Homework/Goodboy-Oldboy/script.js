5b1c620b01c5d9fb27369bba275451383fedd99e
Nusrath <nusrathjahan0204@gmail.com>
2024-06-03 13:37:21 +0200

    added new 2 project

---
+++ JavaScript/JS-Week_1/Javascript_Homework/Goodboy-Oldboy/script.js
@@ -0,0 +1,15 @@
+
+const dogYearOfBirth = 2019;
+const dogYearFuture  = 2030;
+let dogYear = dogYearFuture - dogYearOfBirth ;
+let shouldShowResultInDogYears = true ;
+
+if (shouldShowResultInDogYears) {
+    dogYear = dogYear * 7;
+}
+
+if(shouldShowResultInDogYears){
+    console.log(`My dog will be ${dogYear} dog years old in ${dogYearFuture}`)
+} else {
+    console.log(`My dog will be ${dogYear} human years old in ${dogYearFuture}`)
+}
---
+++ JavaScript/JS-Week_1/Javascript_Homework/Housey Pricey/script.js
@@ -0,0 +1,7 @@
+let wide = 8 ;
+let high = 10 ;
+let deep = 10 ;
+let volumeInMeters =  wide * high * deep;
+let gardenSizeInM2 = 100 ;
+let costOfHouse = 1.000.000 ;
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
