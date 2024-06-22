d1567e37df6aeebd90222d0fe1c6474687b3125d
Nusrath <nusrathjahan0204@gmail.com>
2024-06-22 08:57:17 +0200

    Added week-2 classWork

---
+++ JavaScript-1/JS-Week-2/JS_ClassWork/script.js
@@ -0,0 +1,88 @@
+const students = [
+    {
+        name: 'John',
+        lastName: 'Doe',
+        birthYear: 1990,
+        getAge: function (birthYear) {
+            const currentYear = new Date().getFullYear();
+            const age = currentYear - this.birthYear;
+            console.log(this.lastName);            
+            return age;
+        }
+    },
+    {
+        name: 'Jane',
+        lastName: 'Carpenter',
+        birthYear: 1994,
+        getAge: function () {
+            const age = 30
+            return age;
+        }
+    }
+];
+
+for ( let i = 0; i<students.length; i++){
+    console.log("Student Age", students[i].getAge());
+}
+
+for ( let i = students.length - 1; i >= 0; i-- ){
+    console.log("Student Age Backwards", students[i].getAge());
+}
+//Array
+
+const pet = ["Dog",
+    "Cat"];
+    pet.push("Monkey");
+    pet.unshift("Bird");
+    console.log(pet);
+
+console.log(students[0].getAge());
+
+
+
+function findLongest(str) {
+
+
+
+  
+    var spl = str.findLongest();
+    var longest = 0;
+    
+for (var i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest = spl[i].length 
+      }
+    }
+      return longest;
+    
+    console.log(longest);
+  }
+
+  //-------------------
+  const numbers =  [20, 100, 87, 2, 1, 260, -60, 5, 4];
+
+  function getSmallestNumber(){
+     let smallestNumber = 99999;
+ for (let i = 0; i < numbers.length; i++){
+    const number = numbers[i];
+    if(number < smallestNumber){
+        smallestNumber = number;
+    }
+ }
+ return smallestNumber;
+ 
+  }
+  
+  console.log(getSmallestNumber());
+  //---------------
+ 
+const frequencyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+function getWordFrequency() {
+const words = frequencyText.split(" ");
+console.log(words);
+}
+
+
+ 
+ 
