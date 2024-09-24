9b12f66a6a07a455bfd81bcd2ea84a745fb941f4
Nusrath <nusrathjahan0204@gmail.com>
2024-09-24 10:36:16 +0200

    Some changes have been made to the avg.js file

--- NodeJS/Week-1/avg.js
+++ NodeJS/Week-1/avg.js
@@ -4,8 +4,8 @@ if (process.argv.length <= 2) {
     process.exit(1);
 }
 
-let numbers = process.argv.slice(2).map(Number);
-let sum = numbers.reduce((acc, val) => acc + val, 0);
-let avg = sum / numbers.length;
+const numbers = process.argv.slice(2).map(Number);
+const sum = numbers.reduce((acc, val) => acc + val, 0);
+const avg = sum / numbers.length;
 
 console.log(`The average is: ${avg}`);
